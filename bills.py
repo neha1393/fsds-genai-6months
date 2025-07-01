3e145a246dd3d3398e8bd20bb77f682907a3632b
NehaY <146336325+neha1393@users.noreply.github.com>
2025-07-01 23:15:29 +0530

    Add files via upload

---
+++ bills.py
@@ -0,0 +1,11 @@
+from collections import Counter
+price={'soap':50,'shampoo':25,'toothpaste':20.5,'toothbrush':10.59}
+def generate_bill(cart):
+    total=0
+    for item,qty in cart.items():
+        print(f'{item})
+
+
+
+
+cart=Counter(soap=5,shampoo=2,toothpaste=6,toothbrush=5)
---
+++ chgeck.py
@@ -0,0 +1,9 @@
+s=input("enter a character")
+if len(s)==1:
+    if s.isalpha():
+        print("it is alphabet")
+    else:
+        print("not a alphabet")
+else:
+    print("enter correct details")
+    
